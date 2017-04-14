77381638932a455ce5ec2397aec162e702c107ea
denya_bit <denyabit@gmail.com>
2017-04-14 20:08:58 +0300

    Add product model and repository interface for them

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -17,7 +20,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            //put bidings here
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Addidas Running", Price = 50},
+                new Product { Name = "Surf board", Price = 170},
+                new Product { Name = "New Balance Hiking", Price = 68}
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
 
         public object GetService(Type serviceType)
