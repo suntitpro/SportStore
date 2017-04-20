8769584bec2d7f89a74672095b2dbc3afc3e660b
denya_bit <denyabit@gmail.com>
2017-04-20 18:00:41 +0300

    Definite cart with methods for add, remove, calculate total and clear products

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartDetail> detailCollection = new List<CartDetail>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            var detail = detailCollection
+                .FirstOrDefault(p => p.Product.Id == product.Id);
+
+            if (detail == null)
+            {
+                 detailCollection.Add(new CartDetail {Product = product, Quantity = quantity});
+            }
+            else
+            {
+                detail.Quantity += quantity;
+            }
+        }
+
+        public void RemoveDetail(Product product)
+        {
+            detailCollection.RemoveAll(r => r.Product.Id == product.Id);
+        }
+
+        public decimal CalculateTotalValue()
+        {
+            return detailCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+
+        public void Clear()
+        {
+            detailCollection.Clear();
+        }
+
+        public IEnumerable<CartDetail> Details { get { return detailCollection; } }
+    }
+
+    public class CartDetail
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ SportsStore.UnitTests/Entities/CartTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.UnitTests.Entities
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            //Arrange - create some test products
+            var product1 = new Product { Id = 1, Name = "P1" };
+            var product2 = new Product { Id = 2, Name = "P2" };
+
+            //Arrange - create new cart
+            var target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(product1, 1);
+            target.AddItem(product2, 1);
+            var result = target.Details.ToArray();
+
+            //Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Product, product1);
+            Assert.AreEqual(result[1].Product, product2);
+
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            //Arrange - create some test products
+            var product1 = new Product { Id = 1, Name = "P1" };
+            var product2 = new Product { Id = 2, Name = "P2" };
+
+            //Arrange - create new cart
+            var target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(product1, 1);
+            target.AddItem(product2, 1);
+            target.AddItem(product2, 10);
+            var result = target.Details.ToArray();
+
+            //Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Quantity, 1);
+            Assert.AreEqual(result[1].Quantity, 11);
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            //Arrange - create some test products
+            var product1 = new Product { Id = 1, Name = "P1" };
+            var product2 = new Product { Id = 2, Name = "P2" };
+            var product3 = new Product { Id = 3, Name = "P3" };
+
+            //Arrange - create new cart
+            var target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(product1, 1);
+            target.AddItem(product2, 3);
+            target.AddItem(product3, 5);
+            target.AddItem(product2, 2);
+
+            //Act
+            target.RemoveDetail(product2);
+
+            //Assert
+            Assert.AreEqual(target.Details.Count(c => c.Product == product2), 0);
+            Assert.AreEqual(target.Details.Count(), 2);
+        }
+
+        [TestMethod()]
+        public void Calculate_Cart_Total()
+        {
+            //Arrange - create some test products
+            var product1 = new Product { Id = 1, Name = "P1", Price = 100};
+            var product2 = new Product { Id = 2, Name = "P2", Price = 50};
+            
+            //Arrange - create new cart
+            var target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(product1, 1);
+            target.AddItem(product2, 1);
+            target.AddItem(product2, 2);
+
+            //Act
+            var total = target.CalculateTotalValue();
+
+            //Assert
+            Assert.AreEqual(total, 250);
+        }
+
+        [TestMethod()]
+        public void Can_Clear_Contents()
+        {
+            //Arrange - create some test products
+            var product1 = new Product { Id = 1, Name = "P1", Price = 100 };
+            var product2 = new Product { Id = 2, Name = "P2", Price = 50 };
+
+            //Arrange - create new cart
+            var target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(product1, 1);
+            target.AddItem(product2, 1);
+            
+            //Act
+            target.Clear();
+
+            //Assert
+            Assert.AreEqual(target.Details.Count(), 0);
+        }
+    }
+}
